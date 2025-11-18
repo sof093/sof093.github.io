2eb62effd212ebd2acc406d3259096686b6ca1c3
sof093 <sofiag0029@gmail.com>
2025-11-18 02:40:15 -0600

    modal
    modal

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
